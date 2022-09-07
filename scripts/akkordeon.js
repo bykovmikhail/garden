8e06a5a05c8bad1711d517739b9d8365c2940429
bykovmikhail <bykovmikhail1969@gmail.com>
2022-09-07 09:26:38 +0300

    first commit

---
+++ scripts/akkordeon.js
@@ -0,0 +1,10 @@
+const workName = document.querySelectorAll('[data-name ="work-name"]')
+
+
+workName.forEach(function(item) {
+
+
+	item.addEventListener('click', function(){
+		this.nextElementSibling.classList.toggle('work-diskription-hidden')
+	})
+})
