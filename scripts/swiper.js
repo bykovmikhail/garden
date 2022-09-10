e256b40a060d6fc939a13a510224b92ee9f855b0
bykovmikhail <bykovmikhail1969@gmail.com>
2022-09-10 13:15:44 +0300

    10.09.2022 intrmediate commit

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,23 +1,38 @@
 const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	slideToClickedSlide:true,
-	loop: true,
+   // Optional parameters
+slideToClickedSlide: true,
+   loop: true,
+   /* effect: 'fade' , */
+   /* effect:'flip',
+	flipEffect:{
+		slideShadows: true,
+		limitRotation: true
+	}, */
+   /* mousewheel: {
+      sensitivity: 1,
+      eventsTarget: '.swiper-slide',
+   }, */
+   keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      /* pageUpDown: true */
+   },
+
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
 	
- 
-	// If we need pagination
-	pagination: {
-	  el: '.swiper-pagination',
-	  type: 'fraction',
-	},
- 
-	// Navigation arrows
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
- 
-	// And if we need scrollbar
-	/* scrollbar: {
+
+   // And if we need scrollbar
+   /* scrollbar: {
 	  el: '.swiper-scrollbar',
 	}, */
- });
+});
