83c2c996a50ebc1a968f2ab9496cbea36df39738
bykovmikhail <bykovmikhail1969@gmail.com>
2022-12-11 17:52:45 +0300

    ppp

---
+++ mail/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$recepient = "bykovmikhail1969@gmail.com";
+$siteName = "Бакеев Групп";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Сообщение: $name \nТелефон: $phone";
+
+$pagetitle = "Сообщение с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
