5192cc7a123810a08a0543a0b79665c1ff32f5a1
bykovmikhail <bykovmikhail1969@gmail.com>
2022-12-26 18:45:29 +0300

    26/12/2022

--- mail/mail.php
+++ mail/mail.php
@@ -6,7 +6,7 @@ $siteName = "Бакеев Групп";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$message = "Сообщение: $name \nТелефон: $phone";
+$message = "Телефон: $phone\nСообщение: $name";
 
 $pagetitle = "Сообщение с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
